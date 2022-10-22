7704d441c694f798e6b2be80787dfe3b6f90d3f0
maiyang <yangwen.yw@gmail.com>
2022-10-22 23:50:25 +0800

    feat: support lua http call

--- cdc/sink/lua/scripts/addTable.lua
+++ cdc/sink/lua/scripts/addTable.lua
@@ -5,4 +5,14 @@ function addTable(tableID)
     file:write( "here's a addTable and tableID: " )
     file:write( tableID.."\r\n" )
     file:close()
+
+    local http = require("http")
+
+    -- POST
+    data = "{\"operation\":\"sink_add_table\",  \"data\": {\"table_id\": " .. tableID .. "}}"
+
+    local response, error_message = http.request("POST", "http://localhost:5005/sink_sync", {
+        form=data
+    })
+    print("res: ", response, error_message)
 end
